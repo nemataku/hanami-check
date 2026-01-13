336010a48b20a483dd73769ed8495916e5671b65
Nema Takuya <nematakuya@NemanoMacBook-Pro.local>
2026-01-13 21:34:37 +0700

    fix: sitemap baseUrl undefined

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,38 +1,32 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL!;
+  const baseUrl = "https://hanami-check.vercel.app";
 
   return [
     {
-      url: `${baseUrl}/`,
+      url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1.0,
     },
     {
       url: `${baseUrl}/post`,
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/results`,
+      lastModified: new Date(),
     },
     {
       url: `${baseUrl}/terms`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.3,
     },
     {
       url: `${baseUrl}/privacy`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.3,
     },
     {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.3,
     },
   ];
 }
